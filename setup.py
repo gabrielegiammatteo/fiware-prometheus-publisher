2d5f8109bfc1069d1476a80603e6780c15d51eea
gabriele.giammatteo@eng.it <1Zf1t8Oj>
2019-03-27 15:13:23 +0100

    testing CI deploy on PYPI

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='fiware-prometheus-publisher',
-    version='0.0.36',
+    version='0.0.37',
     description='Ceilometer Publisher for the FIWARE infrastructure',
     long_description=long_description,
     long_description_content_type='text/markdown',
